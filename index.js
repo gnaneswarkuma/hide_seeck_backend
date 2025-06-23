b042f7f29ebaab05d3dc1ab0656f40b47b536985
Gnaneswar kmar <gnaneswarkumar.pithani@gmail.com>
2025-06-23 19:49:37 +0530

    I commit

--- index.js
+++ index.js
@@ -161,6 +161,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("✅ Socket.IO server running on http://localhost:3000");
+const PORT =3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Socket.IO server running on http://0.0.0.0:${PORT}`);
 });
